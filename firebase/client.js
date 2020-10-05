fa436d9ffa711b854e319c9bc16fad24cdceede5
=Jhosua Pachon <jhosuapachon@gmail.com>
2020-10-05 17:36:42 -0500

    update client firebase

--- firebase/client.js
+++ firebase/client.js
@@ -50,21 +50,34 @@ export const addDevit = ({ avatar, content, img, userId, userName }) => {
   });
 };
 
-export const fetchLatestDevits = () => {
+export const listenLatestDevits = (callback) => {
   return db
     .collection("devits")
     .orderBy("createdAt", "desc")
-    .get()
-    .then(({ docs }) => {
-      return docs.map((doc) => {
-        const data = doc.data();
-        const id = doc.id;
-        const { createdAt } = data;
-
-        return { ...data, id, createdAt: +createdAt.toDate() };
-      });
+    .limit(20)
+    .onSnapshot(({ docs }) => {
+      const newDevits = docs.map(mapDevitFromFirebaseToDevitObject);
+      callback(newDevits);
     });
 };
+
+const mapDevitFromFirebaseToDevitObject = (doc) => {
+  const data = doc.data();
+  const id = doc.id;
+  const { createdAt } = data;
+
+  return { ...data, id, createdAt: +createdAt.toDate() };
+};
+
+// export const fetchLatestDevits = () => {
+//   return db
+//     .collection("devits")
+//     .orderBy("createdAt", "desc")
+//     .get()
+//     .then(({ docs }) => {
+//       return docs.map(mapDevitFromFirebaseToDevitObject);
+//     });
+// };
 
 export const uploadImage = (file) => {
   const ref = firebase.storage().ref(`images/${file.name}`);
